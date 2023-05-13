3a7e2df2cf99445340804e8485257a05dadac6ae
Zahra Hojabri <zhojabri@ymail.com>
2023-05-13 19:59:39 +0330

    validate error

--- src/middleware/Validate.js
+++ src/middleware/Validate.js
@@ -1,3 +1,25 @@
-export const Validate = () => {
-  console.log(values);
+export const Validate = (values) => {
+  const {
+    userName,
+    phoneNumber,
+    password,
+    confirmPassword,
+    emailAddress,
+    checkRull,
+  } = values;
+
+  console.log(
+    "userName:",
+    userName,
+    "phoneNumber:",
+    phoneNumber,
+    "password:",
+    password,
+    "confirmPassword:",
+    confirmPassword,
+    "emailAddress:",
+    emailAddress,
+    "checkRull:",
+    checkRull
+  );
 };
